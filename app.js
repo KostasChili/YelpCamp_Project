2595ef4bd59509d0db56701189a81641e72f71bf
KonstantinosTsililis <kotsililis@gmail.com>
2022-09-01 00:05:37 +0300

    in app.js added  an index route for all campgroudns and a show route for campgraound details

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const port = 3000;
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride=require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp-DB');
 
@@ -10,6 +11,8 @@ const db = mongoose.connection; // ?
 db.on('error', console.error.bind(console, "connection error:"));
 db.once('open', () => { console.log("DB connected") });
 const app = express();
+
+app.use(methodOverride('_method'))
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,9 +25,18 @@ app.get('/home', (req, res) => {
     res.render('home.ejs');
 });
 
-app.get('/makecampground', async (req, res) => {
-    
+app.get('/campgrounds', async (req, res) => {
+const campgrounds=await Campground.find({});
+res.render('campgrounds/index.ejs',{campgrounds});
 });
+
+app.get('/campgrounds/:id',async (req,res)=>{
+const {id}= req.params;
+const camp=await Campground.findById(id);
+res.render('campgrounds/show',{camp});
+});
+
+
 
 app.listen(port, () => {
     console.log(`server running at port ${port}`);
