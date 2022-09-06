e7f8cdd467eab64e66409704adcc7059790a6166
KonstantinosTsililis <kotsililis@gmail.com>
2022-09-06 19:05:11 +0300

    Added edit.ejs added edit routes
    Edit form for campground
    
    edit app.get -> serves the form
    edit app.put ->updates  the db

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const port = 3000;
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
-const methodOverride=require('method-override');
+const methodOverride = require('method-override');
 const { urlencoded } = require('express');
+const campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp-DB');
 
@@ -13,8 +14,10 @@ db.on('error', console.error.bind(console, "connection error:"));
 db.once('open', () => { console.log("DB connected") });
 const app = express();
 
-app.use(methodOverride('_method'))
-app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+//middlewere parses the urlencoded  payload
+app.use(express.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,30 +25,47 @@ app.set('view engine', 'ejs');
 
 //index route containing all campgrounds in db
 app.get('/campgrounds', async (req, res) => {
-const campgrounds=await Campground.find({});
-res.render('campgrounds/index.ejs',{campgrounds});
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index.ejs', { campgrounds });
 });
 
 
 //create new campground route - Serves the form to create a new campground via get request
-app.get('/campgrounds/new',(req,res)=>{
+app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
 
 //details page route via campground ID
-app.get('/campgrounds/:id',async (req,res)=>{
-const {id}= req.params;
-const camp=await Campground.findById(id);
-res.render('campgrounds/show',{camp});
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+    res.render('campgrounds/show', { camp });
 });
 
+
 //creates and saves the new camp ground via post request from a form
-app.post('/campgrounds',async(req,res)=>{
-const campground = new Campground(req.body.campground);
-await campground.save();
-res.redirect(`/campgrounds/${campground._id}`);
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 });
+
+//edit campground route - Serves the form to edit via get request
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit.ejs', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const updatedCampground = await Campground.findByIdAndUpdate(id, { ...req.body.campgrounds }, { new: true });
+    console.log(updatedCampground);
+    res.redirect(`/campgrounds/${updatedCampground._id}`)
+});
+
 
 app.listen(port, () => {
     console.log(`server running at port ${port}`);
