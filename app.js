db6d70fac354cb9ff143b639681749ec2a9cb514
KonstantinosTsililis <kotsililis@gmail.com>
2022-11-14 01:50:01 +0200

    added helmet

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ const passport = require('passport');
 const localStrategy = require('passport-local');
 const User  = require('./models/user');
 
+//helmet
+const helmet =require('helmet');
+
 //mongo connection
 mongoose.connect('mongodb://localhost:27017/yelp-camp-DB');
 const db = mongoose.connection; // ?
@@ -46,6 +49,57 @@ app.use(mongoSanitize());
 
 //use flash
 app.use(flash());
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css",
+    "https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css",
+    "https://cdn.jsdelivr.net" 
+]
+;
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+    "https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css"
+];
+const fontSrcUrls = [];
+ app.use(
+     helmet.contentSecurityPolicy({
+         directives: {
+            defaultSrc: [],
+             connectSrc: ["'self'", ...connectSrcUrls],
+             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+             workerSrc: ["'self'", "blob:"],
+             objectSrc: [],
+             imgSrc: [
+                 "'self'",
+                  "blob:",
+                 "data:",
+                 "https://res.cloudinary.com/dq4xdfc6v/", 
+                 "https://images.unsplash.com/",
+               
+             ],
+             fontSrc: ["'self'", ...fontSrcUrls],
+         },
+     })
+ );
 
 
 
@@ -67,6 +121,7 @@ const sessionConfig = {
     //store:mongo for later
 }
 app.use(session(sessionConfig));
+
 
 
 //use of method override package
